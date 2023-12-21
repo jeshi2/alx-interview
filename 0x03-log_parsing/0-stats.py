c4c395e99492e4353b2249e022774a6281491d84
jeshi2 <jamesvean19@gmail.com>
2023-12-21 11:15:10 +0300

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,53 +3,35 @@
 import sys
 
 
-def print_stats(total_size, status_codes):
-    """print stats"""
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes):
-        print("{}: {}".format(code, status_codes[code]))
+if __name__ == '__main__':
 
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-def parse_line(line, total_size, status_codes):
-    """line parsing"""
-    try:
-        elements = line.split()
-        status_code = int(elements[-2])
-        file_size = int(elements[-1])
-
-        """Check if status code is one of the specified values"""
-        if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
-            status_codes[status_code] = status_codes.get(status_code, 0) + 1
-
-        total_size += file_size
-    except (ValueError, IndexError):
-        """Ignore lines that do not match the specified format"""
-        pass
-
-    return total_size, status_codes
-
-
-def main():
-    """call"""
-    total_size = 0
-    status_codes = {}
-    line_count = 0
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
         for line in sys.stdin:
-            total_size, status_codes = parse_line(
-                line.strip(), total_size, status_codes)
-            line_count += 1
-
-            """Print stats every 10 lines"""
-            if line_count % 10 == 0:
-                print_stats(total_size, status_codes)
-
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        """Handle KeyboardInterrupt (Ctrl+C)"""
-        print_stats(total_size, status_codes)
-        sys.exit(0)
-
-
-if __name__ == "__main__":
-    main()
+        print_stats(stats, filesize)
+        raise
