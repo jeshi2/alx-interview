be1ee3d486c1bb43959a8de719785b66177995bc
jeshi2 <jamesvean19@gmail.com>
2024-02-04 15:18:32 +0300

    change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module for making change using the fewest number of coins
+"""
+
+
+def makeChange(coins, total):
+    """
+    makechange
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
