6d1f6c8b712911a1d52472a34e10d3b4e385d703
jeshi2 <jamesvean19@gmail.com>
2024-02-14 13:44:03 +0300

    easy

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,70 +4,34 @@ Primegame
 """
 
 
-def is_prime(num):
-    """
-    Check if a number is prime
-    """
-    if num < 2:
-        return False
-        """
-        Check divisibility from 2 to square root of num
-        """
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-
-def get_primes(n):
-    """
-    Generate a list of prime numbers up to n
-    """
-    primes = []
-    """
-    Iterate from 1 to n (inclusive)
-    """
-    for i in range(1, n + 1):
-        """
-        Check if the number is prime and add it to the list if so
-        """
-        if is_prime(i):
-            primes.append(i)
-    return primes
-
-
 def isWinner(x, nums):
-    """
-    Determine the winner of the game
-    """
-    maria_wins = 0
-    ben_wins = 0
+    def sieve_of_eratosthenes(n):
+        sieve = [True] * (n + 1)
+        sieve[0] = sieve[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if sieve[i]:
+                for j in range(i * i, n + 1, i):
+                    sieve[j] = False
+        return [i for i in range(n + 1) if sieve[i]]
 
-    """
-    Iterate through each round
-    """
-    for n in nums:
-        """
-        Get the list of prime numbers up to n
-        """
-        primes = get_primes(n)
-        """
-        If the number of primes is even, Ben wins
-        """
+    def optimal_strategy(primes):
         if len(primes) % 2 == 0:
-            ben_wins += 1
+            return "Ben"
         else:
-            """
-            Otherwise, Maria wins
-            """
-            maria_wins += 1
+            return "Maria"
 
-    """
-    Compare the number of wins for Maria and Ben
-    """
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+    winners = []
+    for n in nums:
+        primes = sieve_of_eratosthenes(n)
+        winner = optimal_strategy(primes)
+        winners.append(winner)
+
+    ben_wins = winners.count("Ben")
+    maria_wins = winners.count("Maria")
+
+    if ben_wins > maria_wins:
         return "Ben"
+    elif maria_wins > ben_wins:
+        return "Maria"
     else:
         return None
