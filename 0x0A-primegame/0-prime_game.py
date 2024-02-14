85b5bc23525578c6058159cfad10fbbbab85986c
jeshi2 <jamesvean19@gmail.com>
2024-02-14 13:48:29 +0300

    easy

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,41 +5,32 @@ Primegame number
 
 
 def isWinner(x, nums):
-    """
-    generate winner
-    """
-    def sieve_of_eratosthenes(n):
-        """use algorith
-        """
-        sieve = [True] * (n + 1)
-        sieve[0] = sieve[1] = False
-        for i in range(2, int(n ** 0.5) + 1):
-            if sieve[i]:
-                for j in range(i * i, n + 1, i):
-                    sieve[j] = False
-        return [i for i in range(n + 1) if sieve[i]]
-
-    def optimal_strategy(primes):
-        """
-        check optimal
-        """
-        if len(primes) % 2 == 0:
-            return "Ben"
-        else:
-            return "Maria"
-
-    winners = []
-    for n in nums:
-        primes = sieve_of_eratosthenes(n)
-        winner = optimal_strategy(primes)
-        winners.append(winner)
-
-    ben_wins = winners.count("Ben")
-    maria_wins = winners.count("Maria")
-
-    if ben_wins > maria_wins:
-        return "Ben"
-    elif maria_wins > ben_wins:
-        return "Maria"
-    else:
+    """Solves Prime Game"""
+    if not nums or x < 1:
         return None
+    n = max(nums)
+    sieve = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not sieve[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            sieve[j] = False
+
+    sieve[0] = sieve[1] = False
+    c = 0
+    for i in range(len(sieve)):
+        if sieve[i]:
+            c += 1
+        sieve[i] = c
+
+    winner = ''
+    player1 = 0
+    for n in nums:
+        player1 += sieve[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        winner = None
+    if player1 * 2 > len(nums):
+        winner = "Maria"
+    else:
+        winner = "Ben"
+    return winner
