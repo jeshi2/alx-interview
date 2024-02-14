dbc948b7eea4c1b3556be5972645af0a7f1ab3fc
jeshi2 <jamesvean19@gmail.com>
2024-02-14 13:52:33 +0300

    easy peasy

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,33 +4,65 @@ Primegame number
 """
 
 
-def isWinner(x, nums):
-    """Solves Prime Game"""
-    if not nums or x < 1:
+def isWinner(x_rounds, num_list):
+    """
+    Determines the winner of the Prime Game
+    """
+    """
+    Check for invalid inputs
+    """
+    if not num_list or x_rounds < 1:
         return None
-    n = max(nums)
-    sieve = [True for _ in range(max(n + 1, 2))]
-    for i in range(2, int(pow(n, 0.5)) + 1):
-        if not sieve[i]:
+
+    """
+    Find the maximum number in the list
+    """
+    max_num = max(num_list)
+
+    """
+    Initialize a list to mark prime numbers
+    """
+    primes = [True for _ in range(max(max_num + 1, 2))]
+
+    """
+    Use the Sieve of Eratosthenes algorithm to mark non-prime numbers
+    """
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not primes[i]:
             continue
-        for j in range(i * i, n + 1, i):
-            sieve[j] = False
+        for j in range(i * i, max_num + 1, i):
+            primes[j] = False
 
-    sieve[0] = sieve[1] = False
-    c = 0
-    for i in range(len(sieve)):
-        if sieve[i]:
-            c += 1
-        sieve[i] = c
+    """
+    Set 0 and 1 as non-prime
+    """
+    primes[0] = primes[1] = False
 
+    """
+    Count the number of primes up to each index
+    """
+    prime_count = 0
+    for i in range(len(primes)):
+        if primes[i]:
+            prime_count += 1
+        primes[i] = prime_count
+
+    """
+    Determine the winner for each round
+    """
     winner = ''
-    player1 = 0
-    for n in nums:
-        player1 += sieve[n] % 2 == 1
-    if player1 * 2 == len(nums):
+    player1_score = 0
+    for n in num_list:
+        player1_score += primes[n] % 2 == 1
+
+    """
+    Decide the overall winner based on scores
+    """
+    if player1_score * 2 == len(num_list):
         winner = None
-    if player1 * 2 > len(nums):
+    elif player1_score * 2 > len(num_list):
         winner = "Maria"
     else:
         winner = "Ben"
+
     return winner
